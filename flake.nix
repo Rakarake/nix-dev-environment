d6071ef259b3b13bfa574b1977ae36d409b7ec9b
Rakarake <rak@rakarake.xyz>
2024-05-12 18:21:58 +0200

    Opps

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "Neovim environment pinned because the world is on fire";
   inputs = {
+    # Use nixos-unstable as nixpkgs source
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # Wgsl language server
     wgsl_analyzer = {
       url = "github:wgsl-analyzer/wgsl-analyzer";
